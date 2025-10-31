9fa5af932559f18358f07ecddaab58325ee91942
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-10-31 18:03:17 -0300

    feat(generate): crear función generate_controlled_data, para poder generar los 2 dataframes y finalmente llamar a la función

--- src/generate_files.py
+++ src/generate_files.py
@@ -33,3 +33,53 @@ def apply_typo(text: str) -> str:
             return text[:idx] + random.choice(char_pool) + text[idx+1:]
             
     return text
+
+def generate_controlled_data(n_records=50):
+    
+    print(f"Generando {n_records} registros base...")
+    
+    # Generar la data base (Archivo A)
+    base_data: List[Tuple[int, str, str, str]] = []
+    
+    for i in range(n_records):
+        record_id = i + 1
+        #Generar nombres base de compañía y productos
+        company = fake.unique.company()
+        product_name = fake.sentence(nb_words=3)[:-1]
+        year = fake.year()
+        base_data.append((record_id, company, product_name, year))
+        
+    df_a = pd.DataFrame(base_data, columns=['ID', 'Compania', 'Nombre_Producto', 'Anio_Lanzamiento'])
+    
+    # Generar la data de prueba (Archivo B)
+    data_b_list: List[Tuple[int, str, str, str]] = []
+    
+    for index, row in df_a.iterrows():
+        
+        compania_b = apply_typo(row['Compania'])
+        
+        if random.random() < 0.2: 
+            producto_b = row['Nombre_Producto']
+        else: 
+            producto_b = fake.sentence(nb_words=4)[:-1]
+            
+        if random.random() < 0.8:
+            anio_b = row['Anio_Lanzamiento']
+        else:
+            anio_b = str(int(row['Anio_Lanzamiento']) + random.randint(1, 2))
+            
+        data_b_list.append((row['ID'], compania_b, producto_b, anio_b))
+
+    df_b = pd.DataFrame(data_b_list, columns=['ID', 'Compania', 'Nombre_Producto', 'Anio_Lanzamiento'])
+    
+    df_a.to_csv('data/file_a.csv', index=False)
+    df_b.to_csv('data/file_b.csv', index=False)
+    
+    print("✅ Archivos generados exitosamente en la carpeta 'data/':")
+    print(" - 'file_a.csv' (Base de datos)")
+    print(" - 'file_b.csv' (Base de datos con variaciones controladas)")
+
+if __name__ == "__main__":
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    generate_controlled_data(n_records=100)
