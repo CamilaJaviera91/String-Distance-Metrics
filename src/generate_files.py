fd0068e294c69f2ffa9fe78ed11ea96dd0e8e931
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-11-12 10:30:00 -0300

    feat(generate): crear funcion para crear typos

--- src/generate_files.py
+++ src/generate_files.py
@@ -4,3 +4,5 @@ import pandas as pd
 # ---------------------------------------------------
 # Función para generar typos más realistas
 # ---------------------------------------------------
+
+def generar_typo_realista(texto):
