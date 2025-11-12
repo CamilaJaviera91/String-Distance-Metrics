4f48dfeb9eced98a9c41000a4a7f98e54e47e65f
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-11-12 10:30:00 -0300

    feat(generate): agregar comentarios

--- src/generate_files.py
+++ src/generate_files.py
@@ -1,2 +1,6 @@
 import random
 import pandas as pd
+
+# ---------------------------------------------------
+# Función para generar typos más realistas
+# ---------------------------------------------------
