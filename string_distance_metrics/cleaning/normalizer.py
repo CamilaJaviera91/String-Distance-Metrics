28edbeb40f4610d738fb978948e2f83fa2936c53
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-12-29 22:37:23 -0300

    feat(cleaning): agregar variable text

--- string_distance_metrics/cleaning/normalizer.py
+++ string_distance_metrics/cleaning/normalizer.py
@@ -7,3 +7,4 @@ def normalize_string(text: str) -> str:
     
     text = text.lower()
     text = unicodedata.normalize("NFD", text)
+    text = "".join(c for c in text if unicodedata.category(c) != "Mn")
