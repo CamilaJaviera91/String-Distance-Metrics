246f7a1a592cb8b91bce8c05684478181c13b4e1
CamilaJaviera91 <cjmunozn@hotmail.com>
2026-01-04 22:36:49 -0300

    feat(init): agregar manejo de errores en __init__, para uqe las importaciones sean limpias

---
+++ string_distance_metrics/distances/__init__.py
@@ -0,0 +1,3 @@
+from levenshtein import levenshtein_distance
+# Esto permite hacer: from distance import levenshtein_distance
+# en lugar de: from distance.levenshtein_distance import levenshtein_distance
