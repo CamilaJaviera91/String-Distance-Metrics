0f6414a8dd6765fcf2eae1c93c7d248f3966917e
CamilaJaviera91 <cjmunozn@hotmail.com>
2026-01-31 10:00:00 -0300

    feat(clean): cambiar código de normalizacion

--- string_distance_metrics/cleaning/normalizer.py
+++ string_distance_metrics/cleaning/normalizer.py
@@ -1,13 +1,5 @@
 import unicodedata
 
 def normalize_string(text: str) -> str:
-
-    if not isinstance(text, str):
-        return text
-    
-    text = text.lower()
-    text = unicodedata.normalize("NFD", text)
-    text = "".join(c for c in text if unicodedata.category(c) != "Mn")
-    text = " ".join(text.split())
-
-    return text
+    """
+    Normaliza un string para facilitar comparaciones de similitud entre textos.
