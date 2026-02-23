8f4303d9e2e8a81a9087decc14683dceeb1b73d7
CamilaJaviera91 <cjmunozn@hotmail.com>
2026-02-23 10:00:00 -0300

    feat(test): agregar test de jaro

--- tests/test_jaro.py
+++ tests/test_jaro.py
@@ -125,5 +125,7 @@ class TestJaroWinklerDistance:
     def test_sin_coincidencias(self):
         """Strings completamente distintos deben tener distancia 1.0."""
         assert jaro_winkler_distance("abc", "xyz") == 1.0
-
+    
+    def test_complemento_de_similitud(self):
+        
     
