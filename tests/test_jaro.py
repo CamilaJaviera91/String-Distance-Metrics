427e720eed23bab85cb0a83ff85862bcbada7816
CamilaJaviera91 <cjmunozn@hotmail.com>
2026-02-24 10:00:00 -0300

    feat(test): agregar test de jaro

--- tests/test_jaro.py
+++ tests/test_jaro.py
@@ -136,4 +136,5 @@ class TestJaroWinklerDistance:
 
     def test_resultado_en_rango(self):
         """El resultado siempre debe estar en [0.0, 1.0]."""
+        result = jaro_winkler_distance("Balón", "Bqlon")
         
