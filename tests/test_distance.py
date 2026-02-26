39fe51f9cefd508b63d3291278e670dc29669090
CamilaJaviera91 <cjmunozn@hotmail.com>
2026-02-26 10:00:00 -0300

    feat(test2): editar test levenshtein

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -2,4 +2,10 @@ import pytest
 from string_distance_metrics.distances.levenshtein import levenshtein_distance, levenshtein_ratio
 
 class TestLevenshteinDistance:
+    """Tests para la función levenshtein_distance."""
+
+    def test_strings_identicos(self):
+        """Strings idénticos deben tener distancia 0."""
+        assert levenshtein_distance("hola", "hola") == 0
+
     
